06430f1bad501c3ba0d06ef5619a2f366557e47b
Simon Onazi Moses <syflex360@gmail.com>
2021-06-12 17:20:02 +0100

    main api

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ Vue.use(IconsPlugin)
 
 Vue.use(VueAxios, axios)
 
+
 axios.defaults.baseURL = 'https://wakanow.simononazi.com/api/';
 // axios.defaults.baseURL = 'http://wakanow_test_api.test/api';
 
